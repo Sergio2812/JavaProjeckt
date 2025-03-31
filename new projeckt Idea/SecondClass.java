cf7950eaceea2ae8ae47fa3797587418f954f640
Serhii Lebedynets <Lebedynets88@ukr.net>
2025-03-31 12:49:18 +0300

    If-else new commit

--- new projeckt Idea/SecondClass.java
+++ new projeckt Idea/SecondClass.java
@@ -1,5 +1,14 @@
 public class SecondClass {
 public static void main (String[] args) {
 
+    int tsifra = 10;
+     if (tsifra == 10){
+         System.out.println("число 10");
+     }
+     else
+     {
+         System.out.println("число не 10");
+     }
 }
+
 }
--- new projeckt Idea/testClass.java
+++ new projeckt Idea/testClass.java
@@ -1,8 +1,12 @@
 public class testClass {
     public static void main(String [] args) {
         System.out.println("Hello");
-    }
+    int zp;
+    zp = 1500;
+    int zp2 = 2200;
+    System.out.println(zp + 1 + zp2);
 
+    }
 
 }
 
